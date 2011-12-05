51d23784aabc85d9e3362ec7f78182579e457aa3
Brett Kaplan <darklink259@gmail.com>
2011-12-05 15:18:02 -0500

    usage is done

--- memsim.py
+++ memsim.py
@@ -8,8 +8,17 @@ def display_memory(main_mem):
     
 
 def main():
+    
+    if len(sys.argv) != 4 or (sys.argv[1] != "first" and sys.argv[1] != "best" and sys.argv[1] != "next" and sys.argv[1] != "worst"):
+        print "USAGE: memsim.py { first | best | next | worst } <process-termination-probability> <new-process-probability>"
+        return
+    
     main_mem = ['#' for i in range(80)]+[chr(j+65) for j in range(20) for k in range(randint(10,100))]
     main_mem += ['.' for i in range(2400-len(main_mem))]
     display_memory(main_mem)
+    
+    print "Type c and hit ENTER to continue."
+    
+    
     
 main()
