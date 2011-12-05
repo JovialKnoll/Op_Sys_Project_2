aa4c3efbd78ed1896a34470fd5cdc75038a90051
Brett Kaplan <darklink259@gmail.com>
2011-12-05 15:53:52 -0500

    outline of loop is in place

--- memsim.py
+++ memsim.py
@@ -3,22 +3,27 @@
 from random import randint
 import sys
 
+#displays the memory with 20 characters to a line
 def display_memory(main_mem):
     print '\n'.join(map(str, [''.join(map(str, main_mem[i*80:i*80+80])) for i in range(30)]))
-    
 
+#main function
 def main():
-    
+    #checks for proper usage
     if len(sys.argv) != 4 or (sys.argv[1] != "first" and sys.argv[1] != "best" and sys.argv[1] != "next" and sys.argv[1] != "worst"):
-        print "USAGE: memsim.py { first | best | next | worst } <process-termination-probability> <new-process-probability>"
+        print "USAGE: memsim.py { noncontiguous | first | best | next | worst } <process-termination-probability> <new-process-probability>"
         return
-    
+    #initializes memory
     main_mem = ['#' for i in range(80)]+[chr(j+65) for j in range(20) for k in range(randint(10,100))]
     main_mem += ['.' for i in range(2400-len(main_mem))]
     display_memory(main_mem)
-    
-    print "Type c and hit ENTER to continue."
-    
-    
+    #main loop
+    while(True):
+        user_input = raw_input("Type c and hit ENTER to continue.")
+        if user_input == 'q':
+            return
+        else if user_input == 'c':
+            #put stuff here
+            pass
     
 main()
