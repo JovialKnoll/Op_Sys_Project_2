6f47cc7d99f5cf3fcea99ab96cda8ede54d57d54
Brett Kaplan <darklink259@gmail.com>
2011-12-05 14:52:51 -0500

    starting conditions done

--- memsim.py
+++ memsim.py
@@ -1,9 +1,15 @@
 #Brett Kaplan and Jeff Johnston
 
-import random
+from random import randint
 import sys
 
-def main:
-    main_mem = ['#' for i in range(80)]+['.' for i in range(2320)]
+def display_memory(main_mem):
+    print '\n'.join(map(str, [''.join(map(str, main_mem[i*80:i*80+80])) for i in range(30)]))
+    
+
+def main():
+    main_mem = ['#' for i in range(80)]+[chr(j+65) for j in range(20) for k in range(randint(10,100))]
+    main_mem += ['.' for i in range(2400-len(main_mem))]
+    display_memory(main_mem)
     
 main()
