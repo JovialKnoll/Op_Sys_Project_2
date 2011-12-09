8f2551a7f9e4792bd48a6761181e200b7cac346b
Jeff Johnston <johnsj14@rpi.edu>
2011-12-09 17:18:58 -0500

    added placeholder functions for the switch statement.  made defrag work.

--- memsim.py
+++ memsim.py
@@ -10,6 +10,8 @@ def display_memory(main_mem):
     for i in range(30):
         print(''.join(map(str, main_mem[i*80:i*80+80])))
 
+
+
 #main function
 def main():
     
@@ -19,24 +21,60 @@ def main():
     
     
     #checks for proper usage
-    if len(sys.argv) != 4 or (sys.argv[1] != "first" and sys.argv[1] != "best" and sys.argv[1] != "next" and sys.argv[1] != "worst"):
+    if len(sys.argv) != 4:
         print("USAGE: memsim.py { noncontiguous | first | best | next | worst } <process-termination-probability> <new-process-probability>")
         return
+    
     #initializes memory
-    main_mem = ['#' for i in range(80)]
+    main_mem = list(itertools.repeat("#",80))
     for j in range(20):
         letter=next(letters)
         for k in range(randint(10,100)):
             main_mem.append(letter)
     main_mem += ['.' for i in range(2400-len(main_mem))]
+    
     display_memory(main_mem)
+    
+    
     #main loop
     while(True):
-        user_input = input("Type c and hit ENTER to continue.")
+        user_input = input("Type c and hit ENTER to continue. ")
         if user_input == 'q':
             return
         elif user_input == 'c':
-            #put stuff here
+            letter=next(letters)
+            {
+                
+                "noncontiguous": lambda x:noncontiguous_place(main_mem,letter),
+                "first": lambda x:first_place(main_mem,letter),
+                "best": lambda x:best_place(main_mem,letter),
+                "next": lambda x:next_place(main_mem,letter),
+                "worst": lambda x:worst_place(main_mem,letter)
+            }.get(sys.argv[1],-1)(2)
+            
             pass
+
+#these functions are for placing letter into appropriate place in main_mem
+def noncontiguous_place(main_mem,letter):
+    print("noncon")
     
+def first_place(main_mem,letter):
+    print("first")
+    
+def best_place(main_mem,letter):
+    print("best")
+    
+def next_place(main_mem,letter):
+    print("next")
+    
+def worst_place(main_mem,letter):
+    print("worst")
+    
+
+#defragments, but keeps the OS where it belongs
+def defrag(main_mem):
+    nonsys=main_mem[80:]
+    nonsys.sort()
+    return itertools.repeat("#",80)+nonsys
+
 main()
