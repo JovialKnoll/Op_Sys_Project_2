fb0194ca0150f58eda4592449e20d0ac8ba27a85
Jeff Johnston <johnsj14@rpi.edu>
2011-12-09 17:53:23 -0500

    got noncog input working, changes some stuff back to what i had before, fixed defrag and out of memory issues

--- memsim.py
+++ memsim.py
@@ -21,9 +21,8 @@ def main():
     
     
     #checks for proper usage
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 4 or (sys.argv[1] != "first" and sys.argv[1] != "best" and sys.argv[1] != "next" and sys.argv[1] != "worst"):
         print("USAGE: memsim.py { noncontiguous | first | best | next | worst } <process-termination-probability> <new-process-probability>")
-        return
     
     #initializes memory
     main_mem = list(itertools.repeat("#",80))
@@ -43,38 +42,59 @@ def main():
             return
         elif user_input == 'c':
             letter=next(letters)
-            {
-                
-                "noncontiguous": lambda x:noncontiguous_place(main_mem,letter),
-                "first": lambda x:first_place(main_mem,letter),
-                "best": lambda x:best_place(main_mem,letter),
-                "next": lambda x:next_place(main_mem,letter),
-                "worst": lambda x:worst_place(main_mem,letter)
-            }.get(sys.argv[1],-1)(2)
-            
-            pass
+            while letter in main_mem:
+                letter=next(letters)
+            toplace=randint(10,100)
+            worked=switch(main_mem,letter,toplace)
+            if not worked:
+                defrag(main_mem)
+                worked=switch(main_mem,letter,toplace)
+                if not worked:
+                    print("out of memory")
+                    sys.exit()
+        
+        display_memory(main_mem)   
+
+def switch(main_mem,letter,toplace):
+    return {
+        "noncontiguous": lambda x:noncontiguous_place(main_mem,letter,toplace),
+        "first": lambda x:first_place(main_mem,letter,toplace),
+        "best": lambda x:best_place(main_mem,lette,toplacer),
+        "next": lambda x:next_place(main_mem,letter,toplace),
+        "worst": lambda x:worst_place(main_mem,letter,toplace)
+    }[sys.argv[1]](-1)
 
 #these functions are for placing letter into appropriate place in main_mem
-def noncontiguous_place(main_mem,letter):
-    print("noncon")
+def noncontiguous_place(main_mem,letter,toplace):
+    for i in range(80,len(main_mem)):
+        if main_mem[i]==".":
+            main_mem[i]=letter
+            toplace-=1
+        if toplace==0:
+            return 1
+    return 0
     
-def first_place(main_mem,letter):
+def first_place(main_mem,letter,toplace):
     print("first")
+    return 1
     
-def best_place(main_mem,letter):
+def best_place(main_mem,letter,toplace):
     print("best")
+    return 1
     
-def next_place(main_mem,letter):
+def next_place(main_mem,letter,toplace):
     print("next")
+    return 1
     
-def worst_place(main_mem,letter):
+def worst_place(main_mem,letter,toplace):
     print("worst")
+    return 1
     
 
 #defragments, but keeps the OS where it belongs
 def defrag(main_mem):
     nonsys=main_mem[80:]
     nonsys.sort()
-    return itertools.repeat("#",80)+nonsys
+    return list(itertools.repeat("#",80))+nonsys
 
 main()
