6330644ca57161c4075f062351cc9dffcdf31859
Jeff Johnston <johnsj14@rpi.edu>
2011-12-09 16:41:28 -0500

    fixed so compiles on my machine

--- memsim.py
+++ memsim.py
@@ -3,15 +3,16 @@
 from random import randint
 import sys
 
-#displays the memory with 20 characters to a line
+#displays the memory with 80 characters to a line
 def display_memory(main_mem):
-    print '\n'.join(map(str, [''.join(map(str, main_mem[i*80:i*80+80])) for i in range(30)]))
+    for i in range(30):
+        print(''.join(map(str, main_mem[i*80:i*80+80])))
 
 #main function
 def main():
     #checks for proper usage
     if len(sys.argv) != 4 or (sys.argv[1] != "first" and sys.argv[1] != "best" and sys.argv[1] != "next" and sys.argv[1] != "worst"):
-        print "USAGE: memsim.py { noncontiguous | first | best | next | worst } <process-termination-probability> <new-process-probability>"
+        print("USAGE: memsim.py { noncontiguous | first | best | next | worst } <process-termination-probability> <new-process-probability>")
         return
     #initializes memory
     main_mem = ['#' for i in range(80)]+[chr(j+65) for j in range(20) for k in range(randint(10,100))]
@@ -19,10 +20,10 @@ def main():
     display_memory(main_mem)
     #main loop
     while(True):
-        user_input = raw_input("Type c and hit ENTER to continue.")
+        user_input = input("Type c and hit ENTER to continue.")
         if user_input == 'q':
             return
-        else if user_input == 'c':
+        elif user_input == 'c':
             #put stuff here
             pass
     
